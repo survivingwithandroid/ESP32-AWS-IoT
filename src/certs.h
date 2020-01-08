3d32e2b113fc1b5788972e7a53cd4f8b9b300832
Surviving with android (by Francesco Azzola) <survivingwithandroid@gmail.com>
2020-01-08 13:20:07 +0100

    Update certs.h

--- src/certs.h
+++ src/certs.h
@@ -1,11 +1,11 @@
-const char AWS_PUBLIC_CERT[] = "-----BEGIN CERTIFICATE-----\n" \
-"aaaaaaaaaaaaaa\n" \
+const char AWS_PUBLIC_CERT[] = "-----BEGIN CERTIFICATE-----\n"\
+"aaaaaaaaaaaaaa\n"\
 "-----END CERTIFICATE-----";
 
-const char AWS_PRIVATE_KEY[] = "-----BEGIN RSA PRIVATE KEY-----\n" \
-"bbbbbbbbbb\n" \ 
+const char AWS_PRIVATE_KEY[] = "-----BEGIN RSA PRIVATE KEY-----\n"\
+"bbbbbbbbbb\n"\ 
 "-----END RSA PRIVATE KEY-----";
 
-const char AWS_DEVICE_CERT[] = "-----BEGIN CERTIFICATE-----\n" \
-"hhhhhhhhhhh\n" \
+const char AWS_DEVICE_CERT[] = "-----BEGIN CERTIFICATE-----\n"\
+"hhhhhhhhhhh\n"\
 "-----END CERTIFICATE-----\n"; 
